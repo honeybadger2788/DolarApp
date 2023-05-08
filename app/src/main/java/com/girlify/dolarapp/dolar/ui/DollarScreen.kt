dba377debe4f268da1b9d193a6b1ff04a984185e
honeybadger2788 <noe.carosella@gmail.com>
2023-05-07 21:00:08 -0300

    feature: switch dollar operation

--- app/src/main/java/com/girlify/dolarapp/dolar/ui/DollarScreen.kt
+++ app/src/main/java/com/girlify/dolarapp/dolar/ui/DollarScreen.kt
@@ -5,18 +5,27 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.TrendingDown
+import androidx.compose.material.icons.filled.TrendingFlat
+import androidx.compose.material.icons.filled.TrendingUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
@@ -25,11 +34,11 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.girlify.dolarapp.dolar.ui.UiState.*
 import com.girlify.dolarapp.dolar.ui.model.DollarModel
 import com.girlify.dolarapp.dolar.ui.model.DollarOperations
 import com.girlify.dolarapp.dolar.ui.model.DollarOperations.*
@@ -55,16 +64,16 @@ fun DollarScreen(dollarViewModel: DollarViewModel) {
             Footer()
         }
     ) {padding ->
-        val uiState by produceState<UiState>(initialValue = UiState.Loading) {
+        val uiState by produceState<UiState>(initialValue = Loading) {
             dollarViewModel.uiState.collect { value = it }
         }
 
         when(uiState){
-            UiState.Error -> ErrorComponent(msg = "No se pudieron actualizar los valores")
-            UiState.Loading -> LoadingComponent()
-            is UiState.Success -> Body(
+            Error -> ErrorComponent(msg = "No se pudieron actualizar los valores")
+            Loading -> LoadingComponent()
+            is Success -> Body(
                 Modifier.padding(padding),
-                (uiState as UiState.Success).dollars,
+                (uiState as Success).dollars,
                 dollarViewModel
             )
         }
@@ -72,7 +81,6 @@ fun DollarScreen(dollarViewModel: DollarViewModel) {
 }
 
 @Composable
-@Preview(showBackground = true)
 fun Footer() {
     Text(
         text = "Fuente: Ámbito Financiero",
@@ -126,7 +134,7 @@ fun DollarCard(dollars: List<DollarModel>, operationSelected: DollarOperations) 
             mutableStateOf("")
         }
         TextField(
-            value = pesosFormatter(amount),
+            value = inputFormatter(amount),
             onValueChange = {
                 val itWithoutComa = it.replace(",","").trim()
                 amount = itWithoutComa
@@ -162,15 +170,25 @@ fun OperationSelect(operationSelected: DollarOperations, onSelected: (DollarOper
         TextField(
             value = operationToString(operationSelected),
             onValueChange = { onSelected(operationSelected) },
+            label = { Text(text = "Tipo de operación") },
             enabled = false,
             readOnly = true,
+            shape = ShapeDefaults.Medium,
+            textStyle = TextStyle(Color.Black),
             modifier = Modifier
                 .clickable { expanded = true }
-                .fillMaxWidth()
+                .fillMaxWidth(),
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = "down"
+                )
+            }
         )
         DropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false },
+            Modifier.fillMaxWidth()
         ) {
             getOperations().forEach { operation ->
                 DropdownMenuItem(text = {
@@ -187,7 +205,7 @@ fun OperationSelect(operationSelected: DollarOperations, onSelected: (DollarOper
     }
 }
 
-fun pesosFormatter(total: String): String {
+fun inputFormatter(total: String): String {
     val format: NumberFormat = NumberFormat.getInstance()
     format.maximumFractionDigits = 2
     format.maximumIntegerDigits = 12
@@ -209,34 +227,67 @@ fun DollarList(amount: Float, dollars: List<DollarModel>, operationSelected: Dol
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(dollars) {
-            DollarItem(it, amount)
+            DollarItem(it, amount, operationSelected)
         }
     }
 }
 
 @Composable
-fun DollarItem(dollar: DollarModel, amount: Float) {
+fun DollarItem(dollar: DollarModel, amount: Float = 0f, operationSelected: DollarOperations = Compra) {
+    val amountText = when (operationSelected) {
+        Compra -> formatDollarAmount(amount, dollar.sell)
+        Venta -> formatPesoAmount(amount, dollar.buy)
+    }
     Row(
-        Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(text = dollar.name, fontSize = 20.sp, color = Color(0xFF004D40))
+        Row {
+            Text(
+                text = dollar.name,
+                color = Color(0xFF004D40),
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.size(4.dp))
+            VariationIcon(dollar.class_variation)
+            Spacer(modifier = Modifier.size(4.dp))
+            Text(
+                text = dollar.variation,
+                color = Color(0xFF004D40)
+            )
+        }
         Text(
-            text = (if (amount != 0f) {
-                dollarFormatter(amount / dollar.sell.replace(",", ".").toFloat())
-            } else {
-                dollarFormatter(dollar.sell.replace(",", ".").toFloat())
-            }),
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color(0xFF004D40)
+            text = amountText,
+            color = Color(0xFF004D40),
+            fontWeight = FontWeight.Bold
         )
     }
 }
 
-fun dollarFormatter(total: Float): String {
+@Composable
+fun VariationIcon(classVariation: String) {
+    Icon(imageVector =
+        when(classVariation){
+            "up" -> Icons.Default.TrendingUp
+            "down" -> Icons.Default.TrendingDown
+            else -> Icons.Default.TrendingFlat
+    }, contentDescription = "variation")
+}
+
+private fun formatDollarAmount(amount: Float, sellPrice: String): String {
+    val exchangeRate = sellPrice.replace(",", ".").toFloat()
+    val total = if (amount != 0f) amount / exchangeRate else exchangeRate
     val format: NumberFormat = NumberFormat.getCurrencyInstance()
     format.maximumFractionDigits = 2
     format.currency = Currency.getInstance(Locale("en", "US"))
+    return format.format(total)
+}
+
+private fun formatPesoAmount(amount: Float, buyPrice: String): String {
+    val exchangeRate = buyPrice.replace(",", ".").toFloat()
+    val total = if (amount != 0f) amount * exchangeRate else exchangeRate
+    val format: NumberFormat = NumberFormat.getCurrencyInstance()
+    format.maximumFractionDigits = 2
+    format.currency = Currency.getInstance(Locale("es", "AR"))
     return format.format(total)
 }
