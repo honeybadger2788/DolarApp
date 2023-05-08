f38d6cd9a1a7e20526dbe0a3d3078eb7bdf411da
Noelia Carosella <72532638+honeybadger2788@users.noreply.github.com>
2023-05-08 14:39:07 -0300

    feature: notification implemented (#9)

---
+++ app/src/main/java/com/girlify/dolarapp/core/VariationNotification.kt
@@ -0,0 +1,42 @@
+package com.girlify.dolarapp.core
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.girlify.dolarapp.MainActivity
+import com.girlify.dolarapp.R
+
+class VariationNotificacion(
+    var context : Context,
+    var title : String,
+    var msg : String
+) {
+
+    private val channelId = "My_channel"
+    private val channelName= "My channel name"
+    private val notificationManager =
+        context.applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var notificationBuilder: NotificationCompat.Builder
+
+    fun showNotification(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            notificationChannel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        val intent = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        notificationBuilder = NotificationCompat.Builder(context, channelId)
+        notificationBuilder.setSmallIcon(R.drawable.ic_launcher_background)
+        notificationBuilder.addAction(R.drawable.ic_launcher_background, "Open", pendingIntent)
+        notificationBuilder.setContentTitle(title)
+        notificationBuilder.setContentText(msg)
+        notificationBuilder.setAutoCancel(true)
+        notificationManager.notify(100, notificationBuilder.build())
+    }
+}
